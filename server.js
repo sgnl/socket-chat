785215627ebc8dd3a58ca2a07f4e15853c56976f
sgnl <rayrfarias@gmail.com>
2015-10-27 20:00:46 -1000

    add NPM as package manager

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ var server = net.createServer(function(connection) {
   console.log('client connected.');
 
   pool.push(connection);
-  // console.log(connection);
 
   connection.on('data', function(buffer) {
-    pool.forEach(function(socket) {
-      if (socket !== connection) {
-        socket.write(buffer);
+
+    // check for 'commands'
+    commander.inspect(buffer, function(err, parsed){
+      /**
+       * err not implemented
+       */
+
+      if (!parsed.command) {
+        // refactor
+        broadcast(connection, buffer, pool);
+      } else {
+        commander.parse(connection, parsed);
       }
     });
   });
