07419d7368199dab381c135c08e662ba1a83a036
sgnl <rayrfarias@gmail.com>
2015-10-27 20:56:39 -1000

    admin able to broadcast message to all clients

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var net = require('net');
-var commander = require('./lib/commander');
 var CONFIG = require('./config.json');
+var commander = require('./lib/commander');
+var yargs = require('yargs')
+              .usage('Useage: $0 -l [logchat|bool]')
+              .demand([])
+              .argv;
 
 process.stdin.setEncoding('utf8');
 
@@ -9,9 +13,15 @@ var pool = [];
 var server = net.createServer(function(connection) {
   console.log('client connected.');
 
+  // push into connection pool
   pool.push(connection);
 
+  process.stdin.on('data', function(buffer) {
+    connection.write('Admin: ' + buffer.toString());
+  });
+
   connection.on('data', function(buffer) {
+    process.stdout.write('' + connection.username + ': ' + buffer.toString());
 
     // check for 'commands'
     commander.inspect(buffer, function(err, parsed){
@@ -29,10 +39,6 @@ var server = net.createServer(function(connection) {
   });
 });
 
-process.stdin.on('data', function() {
-
-});
-
 server.listen(CONFIG.port, function() {
   console.log('server started.');
 });
@@ -40,7 +46,6 @@ server.listen(CONFIG.port, function() {
 function broadcast (origin, data, pool) {
   pool.forEach(function(socket) {
     if (socket !== origin) {
-      console.log(socket.username);
       socket.write(origin.username + ': ' + data.toString());
     }
   });
